e97e4c79d4a3e0d887bf4d2b75caac7e135559b5
G-Sohal <gnrsohal@gmail.com>
2025-04-07 05:11:51 +0000

    added some to-dos

--- data/constants.py
+++ data/constants.py
@@ -7,5 +7,7 @@
 # - letting blues split into 3
 # - let fortress experience gravity
 # - let birds not be able to go through blocks when string is pulled onto the other side
-
+# - let birds bounce - make bird.velocityy be negative when it hits the ground
+# - add a background music
+# - 
 # - make arenas with different backgrounds, fortress block types, and catapult types
