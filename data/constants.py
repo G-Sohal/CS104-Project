20035933f122b5e06442aa0a78ca18a96181dbc7
G-Sohal <gnrsohal@gmail.com>
2025-04-07 06:09:55 +0000

    removed logs

--- data/constants.py
+++ data/constants.py
@@ -1,5 +1,5 @@
-# make screen_height and screen_width be gloabl and independant
-# other global variables
+# make screen_height and screen_width be gloabl and independant -- not so hard-coded
+# mention other global variables
 
 # TO -DO :
 # - add a pause button
@@ -9,5 +9,7 @@
 # - let birds not be able to go through blocks when string is pulled onto the other side
 # - let birds bounce - make bird.velocityy be negative when it hits the ground
 # - add a background music
-# - 
+# - add trail to birds projectile - if speeded up then dots be conjusted or check accordingly
+# - modularise code a bit more...
+
 # - make arenas with different backgrounds, fortress block types, and catapult types
